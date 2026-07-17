a07a7f43f06340321ce3aaa5ead890bdcf971d80
juliobudiski <juliobudiskiherculani@gmail.com>
2026-07-17 19:24:25 -0300

    feat: Starter kit da base do Streaming

---
+++ src/br/com/streaming/main/MainTeste.java
@@ -0,0 +1,10 @@
+package br.com.streaming.main;
+
+// TODO: Fazer imports das classes dos pacotes model e financeiro
+
+public class MainTeste {
+    public static void main(String[] args) {
+        System.out.println("=== SISTEMA DE STREAMING ===");
+        // TODO: Testes da Aula 2 e 3
+    }
+}
